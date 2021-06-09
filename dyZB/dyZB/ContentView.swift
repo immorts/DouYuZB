de7f4765ec7bf808fee7e0a3e90d3afff26e899d
immorts <3197166390@qq.com>
2021-06-09 14:15:10 +0800

    初始化项目

---
+++ dyZB/dyZB/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  dyZB
+//
+//  Created by Mac on 2021/6/9.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ dyZB/dyZB/dyZBApp.swift
@@ -0,0 +1,17 @@
+//
+//  dyZBApp.swift
+//  dyZB
+//
+//  Created by Mac on 2021/6/9.
+//
+
+import SwiftUI
+
+@main
+struct dyZBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
